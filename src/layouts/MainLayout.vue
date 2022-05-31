4cdfa53743372258e2c8afc8fcac766c30c1d008
Totothelittle <95961236+Totothelittle@users.noreply.github.com>
2022-05-31 08:49:02 +0200

    Adding Quasar framework and begin AlbumView page

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,106 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="bg-primary text-white">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title class="absolute-center">
+          MyPhotoTrip
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      :width="250"
+    >
+      <div class="q-pa-md" style="max-width: 250px">
+        <q-list padding class="text-primary">
+        <q-item-label header>
+          Totothelittle
+        </q-item-label>
+
+          <q-separator spaced />
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'profile'"
+            @click="link = 'profile'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="account_circle" />
+            </q-item-section>
+
+            <q-item-section>Profile</q-item-section>
+          </q-item>
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'settings'"
+            @click="link = 'settings'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="settings" />
+            </q-item-section>
+
+            <q-item-section>Settings</q-item-section>
+          </q-item>
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'help'"
+            @click="link = 'help'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>Help</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+    }
+  }
+})
+</script>
---
+++ src/pages/AlbumView.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="q-pa-md" style="max-width: 350px">
+        <div class="row">
+            <q-list separator>
+                <q-item
+                    v-for="album in albums"
+                    :key= "album.name"
+                    class="q-pa-md" 
+                    clickable
+                    v-ripple
+                >
+                    <q-item-section avatar>
+                        <q-avatar>
+                            <img src="../assets/LogoDefault.png">
+                        </q-avatar>
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>{{ album.name }}</q-item-label>
+                        <q-item-label caption lines="1">{{ album.decription }}</q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
+        </div>
+
+        <div class="column items-center q-pa-md">
+            <q-btn round color="primary" icon="add" dense/>
+                <q-menu>
+                    <p>Hello!</p>
+                </q-menu>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default ({
+  data()
+  {
+    return {
+      albums: [
+        {
+          name: 'Islande',
+          decription: 'L\'album de notre lune de miel !',
+          logo: 'Image'
+        },
+        {
+          name: 'Laponie',
+          decription: 'La visite du village du père Noël',
+          logo: 'Image'
+        },
+        {
+          name: 'Minorque',
+          decription: 'La chaleur des îles',
+          logo: 'Image'
+        }
+      ]
+    }
+  }
+})
+</script>
